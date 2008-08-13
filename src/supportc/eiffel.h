d9592f2dd80252e32d741691159f78a59fa0b6f3
Berend de Boer <berend@pobox.com>
2008-08-13 00:32:57 +0000

    Removed VisualEiffel support.

--- src/supportc/eiffel.h
+++ src/supportc/eiffel.h
@@ -1,32 +1,22 @@
 #ifdef EIFFEL_VENDOR_ge
 #define EIFFEL_VENDOR_GE
 #undef EIFFEL_VENDOR_SE
-#undef EIFFEL_VENDOR_VE
 #undef EIFFEL_VENDOR_ISE
 #else
 #ifdef EIFFEL_VENDOR_se
 #define EIFFEL_VENDOR_SE
-#undef EIFFEL_VENDOR_VE
 #undef EIFFEL_VENDOR_ISE
 #undef EIFFEL_VENDOR_GE
 #else
 #ifdef EIFFEL_VENDOR_ise
 #define EIFFEL_VENDOR_ISE
 #undef EIFFEL_VENDOR_SE
-#undef EIFFEL_VENDOR_VE
 #undef EIFFEL_VENDOR_GE
-#else
-#ifdef EIFFEL_VENDOR_ve
-#define EIFFEL_VENDOR_VE
-#undef EIFFEL_VENDOR_SE
-#undef EIFFEL_VENDOR_ISE
-#undef EIFFEL_VENDOR_GE
-#endif
 #endif
 #endif
 #endif
 
-#if ( (! defined(EIFFEL_VENDOR_SE)) && (! defined(EIFFEL_VENDOR_ISE)) && (! defined(EIFFEL_VENDOR_VE)) && (! defined(EIFFEL_VENDOR_GE)) )
+#if ( (! defined(EIFFEL_VENDOR_SE)) && (! defined(EIFFEL_VENDOR_ISE)) && (! defined(EIFFEL_VENDOR_GE)) )
 #error Eiffel vendor unknown. Please define EIFFEL_VENDOR_XXX.
 #endif
 
@@ -73,24 +63,5 @@
 #endif
 #ifndef EIF_FALSE
 #define EIF_FALSE		(EIF_BOOLEAN) '\0'
-#endif
-#endif
-
-#ifdef EIFFEL_VENDOR_VE
-#include "cecil.h"
-#ifndef EIF_TRUE
-#define EIF_TRUE		(EIF_BOOLEAN) '\01'
-#endif
-#ifndef EIF_FALSE
-#define EIF_FALSE		(EIF_BOOLEAN) '\0'
-#endif
-#ifndef EIF_PROCEDURE
-#define EIF_PROCEDURE EIF_PROC
-#endif
-#ifndef EIF_OBJECT
-#define EIF_OBJECT EIF_REFERENCE
-#endif
-#ifndef NULL
-#define NULL 0
 #endif
 #endif
